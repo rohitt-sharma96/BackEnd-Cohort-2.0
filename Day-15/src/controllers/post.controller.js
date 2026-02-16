bd202e897b685a04267211cbf1ff80a3c9e2078d
Rohit Sharma <sharmarohit469494@gmail.com>
2026-02-16 23:06:50 +0530

    work on getPost & getPostDetails controller

--- Day-15/src/controllers/post.controller.js
+++ Day-15/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require('../models/post.model')
 const imageKit = require('@imagekit/nodejs')
 const { toFile } = require('@imagekit/nodejs')
+const { config } = require('dotenv')
 const jwt = require('jsonwebtoken')
 
 const imagekit = new imageKit({
@@ -59,6 +60,81 @@ async function createPostController(req, res) {
 }
 
 
+async function getPostController(req,res){
+    const token = req.cookies.token
+
+     if(!token){
+            return res.status(401).json({
+                message:"UnAuthorized Access"
+            })
+        }
+
+    let decode;
+    
+    try{
+        decode = jwt.verify(token,process.env.JWT_SECRET)
+    }
+    catch(err){
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+
+    const userId = decode.id
+
+    const posts = await postModel.find({ user:userId })
+
+    res.status(200).json({
+        message:"posts fetched successfully",
+        posts
+    })
+}
 
 
-module.exports = { createPostController }
+
+async function getPostDetailsController(req,res){
+        const token = req.cookies.token
+
+        if(!token){
+            return res.status(401).json({
+                message:"UnAuthorized Access"
+            })
+        }
+
+        let decoded;
+
+        try{
+
+            decoded = jwt.verify(token,process.env.JWT_SECRET)
+        }
+        catch(err){
+            return res.status(401).json({
+                message: "UnAuthorized Token."
+            })
+        }
+
+        const userId = decoded.id
+        const postId = req.params.postId
+        
+        const post = await postModel.findById(postId)
+
+        if(!post){
+            return res.status(404 ).json({
+                message: "post not found"
+            })
+        }
+
+        const isValidUser = post.user.toString() === userId
+
+        if(!isValidUser){
+            return res.status(403).json({
+                message: "Forbidden Content."
+            })
+        }
+        res.status(200).json({
+            message: "post fetched",
+            post
+        })
+}
+
+module.exports = { createPostController, getPostController, getPostDetailsController }
--- Day-15/src/routes/post.routes.js
+++ Day-15/src/routes/post.routes.js
@@ -17,4 +17,21 @@ postRouter.post('/', upload.single('image'), postController.createPostController
 
 
 
+
+/* 
+GET /api/posts  [protected]
+*/
+
+postRouter.get('/',postController.getPostController)
+
+
+
+
+/* 
+GET /api/posts/details/:postId  [protected] <- mtlb token rehna chahiye
+- return a detail about specific post with the id. also check whether the post belongs to the user that the request come from
+*/
+
+postRouter.get('/details/:postId',postController.getPostDetailsController)
+
 module.exports = postRouter;
