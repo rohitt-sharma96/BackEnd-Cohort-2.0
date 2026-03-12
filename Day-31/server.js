cc22f1e3eadf21faa3600d317f80b8aa572234e9
Rohit Sharma <sharmarohit469494@gmail.com>
2026-03-12 22:25:39 +0530

    socket.io with expressJS

---
+++ Day-31/server.js
@@ -0,0 +1,20 @@
+import app from "./src/app.js"
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+    console.log("new connection created")
+    socket.on("message",(msg)=>{
+        console.log("user fire message event")
+        console.log(msg)
+        io.emit("abc", msg)
+    })
+    });
+
+httpServer.listen(3000,()=>{
+    console.log("server is running on port 3000")
+});
