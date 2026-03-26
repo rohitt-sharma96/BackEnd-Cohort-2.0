1d243cd5cb1e4b40f41adcd97872e2d02369dcb0
Rohit Sharma <sharmarohit469494@gmail.com>
2026-03-26 23:12:27 +0530

    send Email with the help of AI

--- Day-34-gen-AI/index.js
+++ Day-34-gen-AI/index.js
@@ -1,7 +1,27 @@
 import "dotenv/config"
 import readLine, { createInterface } from 'readline/promises'
 import { ChatMistralAI } from "@langchain/mistralai";
-import {HumanMessage} from "langchain"
+import { HumanMessage, tool, createAgent} from "langchain"
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod"
+
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name: "emailTool",
+        description: "Use this to send an email",
+        schema:z.object({
+            to: z.string().describe("The recipients email address"),
+            subject:z.string().describe("The subject of an email"),
+            html:z.string().describe("The html content of an email")
+        })
+
+    }
+)
+
+
+
 
 const rl = readLine.createInterface({
     input: process.stdin,
@@ -12,18 +32,27 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest"
 })
 
+const agent = createAgent({
+    model,
+    tools:[emailTool]
+})
+
+
 const messages = []
 
-while(true){
+while (true) {
     const userInput = await rl.question("You: ")
-     
-    messages.push(new HumanMessage(userInput))
-    
-    const response = await model.invoke(messages)
-    
-    messages.push(response)
 
-    console.log("AI: " + response.content)
+    messages.push(new HumanMessage(userInput))
+
+    const response = await agent.invoke({
+        messages
+    })
+
+    messages.push(response.messages[response.messages.length - 1])
+
+    console.log(response)
+    // console.log("AI: " + response.content)
 }
 
 rl.close()
---
+++ Day-34-gen-AI/mail.service.js
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: process.env.GOOGLE_USER,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        clientId: process.env.GOOGLE_CLIENT_ID
+    }
+})
+
+transporter.verify()
+    .then(() => {
+        console.log("Email transporter is ready to send email")
+    })
+    .catch(() => {
+        console.log("Email transporter verification failed")
+    })
+
+
+export async function sendEmail({ to, subject, html, text="" }) {
+
+    const mailOptions = {
+        from: process.env.GOOGLE_USER,
+        to,
+        subject,
+        html,
+        text
+    };
+    const details = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", details)
+    
+    return "Email send successfully to " + to;
+}
